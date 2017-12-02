2ba597a0e660f18a8d4f2f487d8e6f59c379539a
olachudzikiewicz <34106824+olachudzikiewicz@users.noreply.github.com>
2017-12-02 19:55:47 +0100

    Dodano kilka funkcji

--- SandBox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,8 +3,23 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 	
 	public static void main (String[] args ){
-
-		System.out.println("Hello world!");
+    hello("Ola");
+		double l=6;
+    System.out.println("Pole kwadratu o boku " +l + " wynosi: " +area(l));
+    double a=2,b=8;
+    System.out.println("Pole prostokąta o bokach: "+ a + ", " + b + " wynosi: " +area(a,b));
 
 	}
+
+	public static void hello (String somebody) {
+    System.out.println("Hello " + somebody + "!");
+  }
+
+  public static double area (double l) {
+	  return l*l;
+  }
+
+  public static double area (double a, double b) {
+	  return a*b;
+  }
 }
