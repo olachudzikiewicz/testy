a90097779cb16d1adf83b19918ce4ededf43e3ed
olachudzikiewicz <34106824+olachudzikiewicz@users.noreply.github.com>
2017-12-02 20:44:46 +0100

    Zmiana na metody

--- SandBox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,10 +6,10 @@ public class MyFirstProgram {
     hello("Ola");
 
     Square s = new Square(6);
-    System.out.println("Pole kwadratu o boku " + s.l + " wynosi: " +area(s));
+    System.out.println("Pole kwadratu o boku " + s.l + " wynosi: " + s.area());
 
     Rectangle r = new Rectangle(2,3);
-    System.out.println("Pole prostokąta o bokach: "+ r.a + ", " + r.b + " wynosi: " +area(r));
+    System.out.println("Pole prostokąta o bokach: "+ r.a + ", " + r.b + " wynosi: " + r.area());
 
 	}
 
@@ -17,11 +17,5 @@ public class MyFirstProgram {
     System.out.println("Hello " + somebody + "!");
   }
 
-  public static double area (Square s) {
-	  return s.l * s.l;
-  }
 
-  public static double area (Rectangle r) {
-	  return r.a * r.b;
-  }
 }
