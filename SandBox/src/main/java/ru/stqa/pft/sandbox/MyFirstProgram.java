9b3dd1d86c2af0798efd4fd0bd116887d7d12ab2
olachudzikiewicz <34106824+olachudzikiewicz@users.noreply.github.com>
2017-12-02 20:30:02 +0100

    Dodanie klas i obiektów

--- SandBox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,10 +4,12 @@ public class MyFirstProgram {
 	
 	public static void main (String[] args ){
     hello("Ola");
-		double l=6;
-    System.out.println("Pole kwadratu o boku " +l + " wynosi: " +area(l));
-    double a=2,b=8;
-    System.out.println("Pole prostokąta o bokach: "+ a + ", " + b + " wynosi: " +area(a,b));
+
+    Square s = new Square(6);
+    System.out.println("Pole kwadratu o boku " + s.l + " wynosi: " +area(s));
+
+    Rectangle r = new Rectangle(2,3);
+    System.out.println("Pole prostokąta o bokach: "+ r.a + ", " + r.b + " wynosi: " +area(r));
 
 	}
 
@@ -15,11 +17,11 @@ public class MyFirstProgram {
     System.out.println("Hello " + somebody + "!");
   }
 
-  public static double area (double l) {
-	  return l*l;
+  public static double area (Square s) {
+	  return s.l * s.l;
   }
 
-  public static double area (double a, double b) {
-	  return a*b;
+  public static double area (Rectangle r) {
+	  return r.a * r.b;
   }
 }
