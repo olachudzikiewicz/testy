62f2ec8c46eb8d6a3ab2ab75e98fced94a291414
olachudzikiewicz <34106824+olachudzikiewicz@users.noreply.github.com>
2017-12-10 22:37:30 +0100

    Dodanie testu usuwającego grupę i dodanie klasy ogólnej

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTest.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook;
+
+import org.testng.annotations.Test;
+
+
+
+public class GroupDeletionTest extends TestBase {
+
+    @Test
+    public void testGroupDeletion() {
+        gotoGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        returnToGroupPage();
+    }
+
+
+}
