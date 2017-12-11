1b07e4a792c9efcbeb81c23792b11b39072359e4
olachudzikiewicz <34106824+olachudzikiewicz@users.noreply.github.com>
2017-12-11 23:04:16 +0100

    Dodanie testu GroupModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -15,16 +15,5 @@ public class SessionHelper extends HelperBase {
     type(By.name("user"),username);
     type(By.name("pass"),password);
     click(By.xpath("//form[@id='LoginForm']/input[3]"));
-   // wd.findElement(By.name("user")).click();
-    //wd.findElement(By.name("user")).sendKeys("\\undefined");
-   // wd.findElement(By.name("pass")).click();
-//    wd.findElement(By.id("LoginForm")).click();
-   // wd.findElement(By.name("user")).click();
-   // wd.findElement(By.name("user")).clear();
-   // wd.findElement(By.name("user")).sendKeys(username);
-   // wd.findElement(By.name("pass")).click();
-   // wd.findElement(By.name("pass")).clear();
-   // wd.findElement(By.name("pass")).sendKeys(password);
-   // wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupModificationTest.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.test;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTest extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test15", "test2", "test3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
