150286c6ce93470c0305005295e45e1b7f0b7b59
olachudzikiewicz <34106824+olachudzikiewicz@users.noreply.github.com>
2017-12-14 22:17:25 +0100

    Dodanie różnych przeglądarek

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -2,12 +2,14 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class HelperBase {
-  protected FirefoxDriver wd;
 
-  public HelperBase(FirefoxDriver wd) {
+public class HelperBase {
+  protected WebDriver wd;
+
+  public HelperBase(WebDriver wd) {
     this.wd=wd;
   }
 
