e34e7bd8db67f47d9f28170a242189ec6834c29c
olachudzikiewicz <34106824+olachudzikiewicz@users.noreply.github.com>
2017-12-28 22:04:21 +0100

    Modyfikacja testu GroupCreationTest (dodanie sprawdzenia)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -13,9 +14,23 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("test_null1", null, null));
+    GroupData group = new GroupData("test_max_id", null, null);
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1); //sprawdzenie czy rozmiar grupy przed i po się zgadza
+
+
+
+    int max = 0;
+    for (GroupData g:after) {
+      if (g.getId() > max) {
+        max = g.getId();
+      }
+    }
+    group.setId(max);
+    before.add(group); //dodanie do starej listy grupę która została stworzona
+    //przekształcamy listy w zbiory, żeby móc je porównywać:
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 }
