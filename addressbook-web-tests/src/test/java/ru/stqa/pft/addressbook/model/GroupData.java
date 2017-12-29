1892573dbda58482406730634b44796ad5b10703
olachudzikiewicz <34106824+olachudzikiewicz@users.noreply.github.com>
2017-12-29 20:54:57 +0100

    Sortowanie list

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -7,9 +7,8 @@ public class GroupData {
   private final String header;
   private final String footer;
 
-
-   public GroupData(String name, String header, String footer) {
-    this.id = 0;
+  public GroupData(String name, String header, String footer) {
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -50,6 +49,7 @@ public class GroupData {
             '}';
   }
 
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
@@ -57,15 +57,12 @@ public class GroupData {
 
     GroupData groupData = (GroupData) o;
 
-    if (id != groupData.id) return false;
     return name != null ? name.equals(groupData.name) : groupData.name == null;
   }
 
   @Override
   public int hashCode() {
-    int result = id;
-    result = 31 * result + (name != null ? name.hashCode() : 0);
-    return result;
+    return name != null ? name.hashCode() : 0;
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -26,10 +27,15 @@ public class GroupCreationTests extends TestBase {
       }
     } */
 
-    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+ //   group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group); //dodanie do starej listy grupę która została stworzona
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before,after);
+
     //przekształcamy listy w zbiory, żeby móc je porównywać:
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    //Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 }
