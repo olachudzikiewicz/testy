50b74558de4ef96f002b6679ce854c933260980e
olachudzikiewicz <34106824+olachudzikiewicz@users.noreply.github.com>
2018-01-09 20:44:06 +0100

    Dodane keszowanie

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTests.java
@@ -28,8 +28,6 @@ public class GroupCreationTests extends TestBase {
 
     assertThat(after,
             equalTo(before.withAdded(group.withId(after.stream().mapToInt((g)-> g.getId()).max().getAsInt())))); //porównanie dwóch obiektów
-
-
   }
 
 }
